598d64994d27e1680c0e06a21d878000b8a99daf
Adarsh Raghav <52413705+adarshraghav@users.noreply.github.com>
2020-09-30 10:58:46 +0530

    Add files via upload

---
+++ Mobile App/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:accepted/on_boarding.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false, home: Scaffold(body: Onboarding())));
+}
